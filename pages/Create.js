6b2c5f39974693c2ee8d8c94eb73d6563b3e2511
KayeQuamayCredo <120432526+KayeQuamayCredo@users.noreply.github.com>
2022-12-15 20:28:12 +0800

    Add files via upload

---
+++ pages/Create.js
@@ -0,0 +1,11 @@
+import Upload from "../components/Upload";
+import Navbar1 from "../components/Navbar1";
+
+export default function create() {
+  return (
+    <div>
+      <Navbar1 />
+      <Upload />
+    </div>
+  )
+}
